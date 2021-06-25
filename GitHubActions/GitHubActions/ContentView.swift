552777a4f847dabf69863b0656faa2e3c3ec523a
masaki.mizuno <masaki.mizuno@proaxia-consulting.co.jp>
2021-06-25 14:50:17 +0900

    初回コミット

---
+++ GitHubActions/GitHubActions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubActions
+//
+//  Created by MASAKI MIZUNO on 2021/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubActions/GitHubActions/GitHubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsApp.swift
+//  GitHubActions
+//
+//  Created by MASAKI MIZUNO on 2021/06/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
